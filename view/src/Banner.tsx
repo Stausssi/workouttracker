fc2a68b8d8a6d1503a2e4ac350a8b6afe9fb630d
Simon <stauss.simon.01@gmail.com>
2021-03-26 12:10:33 +0100

    added alexis frontend structure

---
+++ view/src/Banner.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "bulma";
+
+class Banner extends React.Component {
+  render() {
+    return (
+      <section className="hero is-primary is-fullheight">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <h1 className="title">Hello, Bulma!</h1>
+            <h2 className="subtitle">This is react app feat. Bulma</h2>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Banner;
