90242c44e136aa68225f0cbb40b9844d81cc2097
Simon <stauss.simon.01@gmail.com>
2021-03-26 12:10:33 +0100

    added alexis frontend structure

---
+++ view/src/Profil.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Head from './Head';
+import Foot from './Foot';
+
+class Profil extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            value: 0,
+            text: '',
+        };
+    }
+    render() {
+        return (
+
+            <section className='main'>
+                <Head></Head>
+                <Foot></Foot>
+            </section>
+        )
+    }
+}
+
+export default Profil
+
