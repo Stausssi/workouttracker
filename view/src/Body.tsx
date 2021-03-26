8e268a5cc791af7b0a044d3548f29e001101ba13
Simon <stauss.simon.01@gmail.com>
2021-03-26 12:10:33 +0100

    added alexis frontend structure

---
+++ view/src/Body.tsx
@@ -0,0 +1,13 @@
+import './css/App.css';
+
+export const Body = () =>
+<section id="body-content">
+<div id="col-1">
+  <h1>This is half of a page</h1>
+</div>
+<div id="col-2">
+  <h1>This is another half of a page</h1>
+</div>
+</section>
+export default Body;
+
