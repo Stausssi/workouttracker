c9354cbc8809905ce17982946db26d48cda9a71c
Alexis Caruyer <a.caruyer@dymatrix.de>
2021-04-04 16:29:00 +0200

    Add events to Calendar and configure charts

--- controller/controllers/calendarController.js
+++ controller/controllers/calendarController.js
@@ -1,61 +1,107 @@
 const { request } = require('express');
-const Event = require("../../model/calendarModel");
-
-//TODO: Add user for event
+const Event = require("../model/calendarModel");
 
 exports.findAll = (request, response) => {
-  let username = request.username;
-  Event.getAll((error, data) => {
-    if (error) {
-      console.log(error);
-      response.sendStatus(500)
-    } else {
-      response.status(200).send({ body: JSON.stringify(data) });
-    }
-  });
+    Event.getAll((error, data) => {
+        if (error) {
+            console.log(error);
+            response.status(500).send({message: "Internal server error!"});
+        } else{
+            response.send(data);
+        } 
+      });
 };
 
 //Create new event and add it to DB
 exports.create = (request, response) => {
-  if (!request.body) {
-    response.sendStatus(400)
-  }
+    if (!request.body) {
+      response.status(400).send({
+        message: "Content can not be empty!"
+      });
+    }
   else {
-    let username = request.username;
     // constructor for event
     const event = new Event({
       title: request.body.title,
       start: request.body.start,
       end: request.body.end,
-      allDay: request.body.allDay,
-      //user:username
+      allDay: request.body.allDay
     });
+    //response.status(200).send("test")
     // Save event in the database
-    Event.create(event, (error, added) => {
-      if (error || !added) {
-        response.sendStatus(500)
-      } else {
-        response.sendStatus(200)
+    Event.create(event, (error, data) => {
+      if (error) {
+            console.log(error);
+            console.log(data)
+            response.status(500).send({message: "Internal server error!"});
+        } else {
+          response.send(data);
       }
     });
-  }
-};
+}   
+  };
 
-exports.remove = (request, response) => {
-  const eventid = request.body.id
-  let username = request.username;
-  if (!eventid/* || !username */) {
-    response.sendStatus(400)
-  }
-  else {
-    Event.remove(eventid, (error, data) => {
-      if (error || !data) {
-        response.sendStatus(500)
-      } else {
-        response.sendStatus(200)
+
+  exports.update = (request, response) => {
+    if (!request.body) {
+      response.status(400).send({
+        message: "Content can not be empty!"
+      });
+    }
+    else {
+      // update an event
+    //TODO
+   /* Event.updateById = (id, customer, result) => {
+      sql.query(
+        "UPDATE events SET title = ?, start = ?, end = ? allDay = ? WHERE id = ?",
+        [event.title, event.start, event.end, event.allDay, id],
+        (error, res) => {
+          if (error) {
+            console.log(error);
+            response.status(500).send({message: "Internal server error!"});
+          }
+          else
+          {
+    
+          if (res.affectedRows == 0) {
+            // not found Customer with the id
+            result({ kind: "not_found" }, null);
+            response.status(404).send({message: "event was not found"});
+          }
+    
+          console.log("updated event: ", { id: id, ...event });
+          result(null, { id: id, ...event });
+        }
       }
-    })
+      );
+    };*/
+    }   
+  };
 
-  }
+  exports.remove = (request, response) => {
+    if (!request.body) {
+      response.status(400).send({
+        message: "Content can not be empty!"
+      });
+    }
+  else {
+    // update an event
+    //TODO
+   /* sql.query("DELETE FROM customers WHERE id = ?", id, (error, response) => {
+      if (error) {
+        console.log(error);
+        response.status(500).send({message: "Internal server error!"});
+      }
+  
+      if (response.affectedRows == 0) {
+        // not found Customer with the id
+        result({ kind: "not_found" }, null);
+        response.status(404).send({message: "event was not found"});
+      }
+  
+      console.log("deleted customer with id: ", id);
+      result(null, error);
+    });*/
+    
+  }   
 };
-
--- controller/controllers/chartController.js
+++ controller/controllers/chartController.js
@@ -1,87 +1,25 @@
 const { request } = require('express');
-const Chart = require("../../model/chartModel");
-const Data = require("../../model/dataModel");
+const Chart = require("../model/chartModel");
+const Data = require("../model/dataModel");
 
 exports.findAll = (request, response) => {
-  let username = request.username;
-    Chart.getAll((error, charts) => {
+    Chart.getAll((error, data) => {
         if (error) {
             console.log(error);
-            response.sendStatus(500)
+            response.status(500).send({message: "Internal server error!"});
         } else{
-            response.status(200).send({body: JSON.stringify(charts)});
+            response.send(data);
         } 
       });
 };
 
-/* Get Data: check if value for user or sport was set and make call to DB depending on it
-    Set params into url
-*/
-
-exports.getdataset = (request,response) => {
-    let username = request.username;
-    let year,sport,category = null
-    sport = request.query.sport
-    category=request.query.category
-    year=request.query.year
-
-    Data.getall(category,sport,year,(error, data) => {
+exports.test = (request, response) => {
+    Data.getdurationpermonth((error, data) => {
         if (error) {
             console.log(error);
-            response.sendStatus(500)
+            response.status(500).send({message: "Internal server error!"});
         } else{
-            response.status(200).send({body: JSON.stringify(data)});
+            response.send(data);
         } 
       });
-};    
-
-
-exports.create = (request, response) => {
-    if (!request.body) {
-        response.sendStatus(400)
-      }
-    else {
-      let username = request.username;
-        const chart = new Chart({
-            name: request.body.name,
-            type: request.body.type,
-            category: request.body.category,
-            fill: request.body.fill,
-            param_sport: request.body.param_sport,
-            year:request.body.year
-            //user:request.username
-          });
-    Chart.create(chart,(error, added) => {
-        if (error) {
-            console.log(error);
-            return response.sendStatus(500)
-        } else{
-            if (added) {
-                response.sendStatus(200)
-            }
-            else {
-                response.sendStatus(500)
-            }
-        } 
-    });
-}
 };
-
-exports.remove = (request, response) => {
-    const chartid = request.body.chartid
-    if (!chartid) {
-      response.sendStatus(400)
-    }
-    else {
-      let username = request.username;
-      Chart.remove(chartid, (error, data) => {
-        if (error) {
-          response.sendStatus(500)
-        } else {
-          response.status(200)
-  
-        }
-      })
-  
-    }
-  };
