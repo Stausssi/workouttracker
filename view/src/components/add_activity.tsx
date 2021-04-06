02ad02da700a893ff3f8b42f956f4979dafcf9b4
Simon <stauss.simon.01@gmail.com>
2021-04-06 22:14:41 +0200

    added dynamic mustParams dependency

--- view/src/components/add_activity.tsx
+++ view/src/components/add_activity.tsx
@@ -28,7 +28,11 @@ const defaultActivityState = {
     heartRate: 0,
     heartRateClass: "",
     heartRateIcon: faTimes,
-    heartRateIconClass: ""
+    heartRateIconClass: "",
+    altitude: 0,
+    altitudeClass: "",
+    altitudeIcon: faTimes,
+    altitudeIconClass: ""
 }
 
 const defaultNotifyState = {
@@ -44,10 +48,12 @@ const validValues: { [key: string]: any[] } = {
     "sport": [0, null],
     "duration": [0, null],
     "distance": [0, null],
+    "pace": [0, null],
     "heartRate": [25, 250],
+    "altitude": [0, null]
 }
 
-const hasIcon: string[] = ["duration", "distance", "heartRate"];
+const hasIcon: string[] = ["duration", "distance", "heartRate", "altitude"];
 
 enum RESET_TYPES {
     ACTIVITY,
@@ -57,13 +63,22 @@ enum RESET_TYPES {
 const notifyMessages: { [ident: string]: [message: string, type: string] } = {
     "fetchFailed":
         ["Die Sportarten konnten nicht abgerufen werden!<br /> Versuche es später erneut, oder kontaktiere einen Administrator.", "is-danger"],
+    "success":
+        ["Die Aktivität wurde erfolgreich gespeichert!", "is-success"]
 }
 
+const NUM_FIELDS = 5;
+const inputFields: string[] = ["distance", "duration", "pace", "heartRate", "altitude"];
+
 export default class AddActivity extends Component<Props, State> {
+    HTMLFields: { [field: string]: JSX.Element; } | undefined;
+    mustParams: boolean[] = [];
+
     constructor(props: Props) {
         super(props);
         this.state = Object.assign({}, defaultState);
 
+        // bind this to event handlers
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleReset = this.handleReset.bind(this);
@@ -86,6 +101,13 @@ export default class AddActivity extends Component<Props, State> {
                 });
             }
         }
+
+        // Check for state changes
+        if (prevState.sport !== this.state.sports) {
+            if (!isNaN(this.state.sports)) {
+                this.mustParams = [];
+            }
+        }
     }
 
     handleChange(event: any) {
@@ -95,7 +117,7 @@ export default class AddActivity extends Component<Props, State> {
 
         // ignore negative values
         if (value < 0) {
-            return
+            return;
         }
 
         // Update state
@@ -130,11 +152,13 @@ export default class AddActivity extends Component<Props, State> {
         // Prevent page refresh
         event.preventDefault();
 
+        // Prevent false submits
         if (this.state.submitButton.hasAttribute("disabled")) return;
 
         console.log("Submit:", this.state);
 
         // TODO: Filter params and insert into database
+        // TODO: reset state params which are neither must nor optional
 
         // fetch("http://localhost:9000/backend/activity/add", {
         //     method: "POST",
@@ -150,7 +174,8 @@ export default class AddActivity extends Component<Props, State> {
         //         this.setState({notifyMessage: "Activity submitted", notifyType: "is-success"});
         //     }
         // });
-        this.setState({notifyMessage: "Activity submitted", notifyType: "is-success"});
+        this.setState({notifyMessage: notifyMessages["success"][0], notifyType: notifyMessages["success"][1]});
+
         // Disable submit button and reset form
         this.allowSubmit(false);
         this.resetState(RESET_TYPES.ACTIVITY);
@@ -173,42 +198,36 @@ export default class AddActivity extends Component<Props, State> {
                 <label className="label">Art der Aktivität</label>
                 <div className={`select is-fullwidth mb-5 ${this.state.sportClass}`}>
                     <select name="sport" onChange={this.handleChange} value={this.state.sport}>
-                        {this.state.notifyMessage !== notifyMessages["fetchFailed"][0] ?
-                            this.createSportSelect() :
-                            <option key="-1" value="-1">Es ist ein Fehler aufgetreten!</option>}
+                        {
+                            this.state.notifyMessage !== notifyMessages["fetchFailed"][0] ?
+                                this.createSportSelect() :
+                                <option key="-1" value="-1">Es ist ein Fehler aufgetreten!</option>
+                        }
                     </select>
                 </div>
 
-                <label className="label">Dauer</label>
-                <div className="columns">
-                    <div className="column">
-                        <div className="field">
-                            <div className="control has-icons-right">
-                                <input
-                                    className={`input ${this.state.durationClass}`}
-                                    type="number"
-                                    name="duration"
-                                    placeholder="Gebe hier die Dauer der Aktivität an"
-                                    value={Number(this.state.duration) === 0 ? "" : this.state.duration}
-                                    onChange={this.handleChange}
-                                />
-                                <span className={`icon is-right ${this.state.durationIconClass}`}>
-                                    <FontAwesomeIcon icon={this.state.durationIcon}/>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="column is-2">
-                        <div className="select is-fullwidth">
-                            <select name="durationMul" onChange={this.handleChange} value={this.state.durationMul}>
-                                <option value="1">s</option>
-                                <option value="60">m</option>
-                                <option value="3600">h</option>
-                            </select>
-                        </div>
-                    </div>
-                </div>
+                {this.createFormFields()}
+            </form>
+        );
+    }
 
+    createSportSelect() {
+        let sports = [];
+        sports.push(<option value="0" key="0"/>);
+
+        for (let key in this.props.sports) {
+            sports.push(<option value={key} key={key}>{key}</option>);
+        }
+
+        return sports;
+    }
+
+    createFormFields() {
+        console.log("forms Select");
+        // Create HTML Fields template
+        // Executed on the client -> no performance problem for now
+        this.HTMLFields = {
+            "distance": <>
                 <label className="label">Distanz</label>
                 <div className="columns">
                     <div className="column">
@@ -218,7 +237,7 @@ export default class AddActivity extends Component<Props, State> {
                                     className={`input ${this.state.distanceClass}`}
                                     type="number"
                                     name="distance"
-                                    placeholder="Gebe hier die zurückgelegte Distanz an"
+                                    placeholder="Gebe hier die zurückgelegte Distanz ein"
                                     value={Number(this.state.distance) === 0 ? "" : this.state.distance}
                                     onChange={this.handleChange}
                                 />
@@ -232,13 +251,48 @@ export default class AddActivity extends Component<Props, State> {
                         <div className="select is-fullwidth">
                             <select className="select" name="distanceMul" onChange={this.handleChange}
                                     value={this.state.distanceMul}>
-                                <option value="1">m</option>
-                                <option value="1000">km</option>
+                                <option value="1" key="dist_m">m</option>
+                                <option value="1000" key="dist_km">km</option>
                             </select>
                         </div>
                     </div>
                 </div>
-
+            </>,
+            "duration": <>
+                <label className="label">Dauer</label>
+                <div className="columns">
+                    <div className="column">
+                        <div className="field">
+                            <div className="control has-icons-right">
+                                <input
+                                    className={`input ${this.state.durationClass}`}
+                                    type="number"
+                                    name="duration"
+                                    placeholder="Gebe hier die Dauer der Aktivität ein"
+                                    value={Number(this.state.duration) === 0 ? "" : this.state.duration}
+                                    onChange={this.handleChange}
+                                />
+                                <span className={`icon is-right ${this.state.durationIconClass}`}>
+                                    <FontAwesomeIcon icon={this.state.durationIcon}/>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="column is-2">
+                        <div className="select is-fullwidth">
+                            <select name="durationMul" onChange={this.handleChange} value={this.state.durationMul}>
+                                <option value="1" key="dur_s">sec</option>
+                                <option value="60" key="dur_m">min</option>
+                                <option value="3600" key="dur_h">h</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </>,
+            "pace": <>
+                <label className="label">Placeholder für Pace -&gt; Wird berechnet?</label>
+            </>,
+            "heartRate": <>
                 <label className="label">Herzrate</label>
                 <div className="field">
                     <div className="control has-icons-right">
@@ -246,7 +300,7 @@ export default class AddActivity extends Component<Props, State> {
                             className={`input ${this.state.heartRateClass}`}
                             type="number"
                             name="heartRate"
-                            placeholder="Gebe hier deine durchschnittliche Herzrate an"
+                            placeholder="Gebe hier deine durchschnittliche Herzrate ein"
                             value={Number(this.state.heartRate) === 0 ? "" : this.state.heartRate}
                             onChange={this.handleChange}
                         />
@@ -255,26 +309,88 @@ export default class AddActivity extends Component<Props, State> {
                         </span>
                     </div>
                 </div>
-            </form>
-        );
-    }
+            </>,
+            "altitude": <>
+                <label className="label">Höhenmeter</label>
+                <div className="field">
+                    <div className="control has-icons-right">
+                        <input
+                            className={`input ${this.state.altitudeClass}`}
+                            type="number"
+                            name="altitude"
+                            placeholder="Gebe hier deine Höhenmeter ein"
+                            value={Number(this.state.altitude) === 0 ? "" : this.state.altitude}
+                            onChange={this.handleChange}
+                        />
+                        <span className={`icon is-right ${this.state.altitudeClass}`}>
+                            <FontAwesomeIcon icon={this.state.altitudeIcon}/>
+                        </span>
+                    </div>
+                </div>
+            </>
+        };
 
-    createSportSelect() {
-        let sports = [];
-        sports.push(<option value="0" key="0"/>);
+        if (this.state.sportClass === "is-success") {
+            // Get bitfields and split them into their bitwise representation
+            // reverse to fill zeros in the beginning
+            let bitfieldArray: any = this.props.sports[this.state.sport];
+            let must: boolean[] = bitfieldArray[1].toString(2).split("").reverse();
+            let optional: boolean[] = bitfieldArray[0].toString(2).split("").reverse();
 
-        for (let key in this.props.sports) {
-            sports.push(<option value={key} key={key}>{key}</option>);
+            // Fill missing zeros
+            for (let i = 0; i < NUM_FIELDS - must.length; i++) {
+                must.push(false);
+            }
+
+            for (let i = 0; i < NUM_FIELDS - optional.length; i++) {
+                optional.push(false);
+            }
+
+            // reverse back
+            must = must.reverse();
+            optional = optional.reverse();
+
+            let fieldsHTML = [<div className="divider">Plichtangaben</div>];
+            for (let index in must) {
+                must[index] = Boolean(Number(must[index]));
+
+                if (must[index]) {
+                    fieldsHTML.push(this.HTMLFields[inputFields[index]]);
+                }
+            }
+
+            // Update must params
+            this.mustParams = must;
+
+            // TODO: Only have optional params that arent in must
+
+            fieldsHTML.push(<div className="divider">Optional</div>);
+            for (let index in optional) {
+                optional[index] = Boolean(Number(optional[index]));
+
+                if (optional[index]) {
+                    //fieldsHTML.push(this.HTMLFields[inputFields[index]]);
+                }
+            }
+
+            return fieldsHTML;
         }
-        return sports;
+        return <p>Bitte wähle eine Sportart aus.</p>;
     }
 
     validateInput() {
-        let valid =
-            this.isValid("sport", this.state.sport) &&
-            this.isValid("distance", this.state.distance) &&
-            this.isValid("duration", this.state.duration) &&
-            this.isValid("heartRate", this.state.heartRate);
+        let valid = false;
+
+        if (this.mustParams.length === NUM_FIELDS) {
+            valid = this.isValid("sport", this.state.sport);
+
+            for (let i = 0; i < NUM_FIELDS; ++i) {
+                if (this.mustParams[i]) {
+                    let param = inputFields[i];
+                    valid = valid && this.isValid(param, this.state[param]);
+                }
+            }
+        }
 
         if (this.state.submitButton) {
             this.allowSubmit(valid);
