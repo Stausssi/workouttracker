71f30311b8832f26fc068cc70c12fe2a2510ac56
Alexis Caruyer <a.caruyer@dymatrix.de>
2021-04-08 21:53:05 +0200

    create api call for events

--- view/src/Head.tsx
+++ view/src/Head.tsx
@@ -9,13 +9,44 @@ import { Link } from "react-router-dom";
 
 export const Head = () =>
 <div id="head">
-<section className="hero is-dark ">
-  <div className="hero-head">
-    <nav className="navbar">
+<section className="hero is-dark is-small">
+    <div className="hero-head">
+      <nav className="navbar">
         <div className="navbar-brand">
-          <a className="navbar-item" href="/Second">
-            <img src={Logo}alt="Logo" width={150}/>
-          </a>
+            <a className="navbar-item" href="/Second">
+              <img src={Logo}alt="Logo" width={150}/> 
+            </a>
+        </div>
+        <div className="navbar-end">
+                    <div className="navbar-item">
+                        <Search/>
+                    </div>
+                    <a className="navbar-item" href="/">
+                        <span className="icon">
+                              <FontAwesomeIcon icon={faHome}/>
+                        </span>
+                        <div>Home</div>
+                    </a>
+                    <a className="navbar-item" href="/Profil">
+                        <span className="icon">
+                              <FontAwesomeIcon icon={faUser}/>
+                        </span>
+                        <div>Profil</div>
+                    </a>
+                    <div className="navbar-item">
+                        <Modal> </Modal>
+                    </div>
+                    <span className="navbar-item">
+
+                    <Link className="button is-primary is-inverted" to={'/login'}>
+                            <span className="icon">
+                              <FontAwesomeIcon icon={faSignInAlt}/>
+                            </span>
+                            <span>Login</span>
+                            </Link>
+                    </span>
+                </div>
+            </nav>
         </div>
         <div className="hero-body">
             <div className="container has-text-centered">
@@ -27,55 +58,6 @@ export const Head = () =>
                 </p>
             </div>
         </div>
-        <div className="hero-foot">
-            <nav className="tabs">
-                <ul>
-                    <li className="is-active"><a href="/#">Overview</a></li>
-                    <li><a href="/#">Modifiers</a></li>
-                    <li><a href="/#">Grid</a></li>
-                    <li><a href="/#">Elements</a></li>
-                    <li><a href="/#">Components</a></li>
-                    <li><a href="/#">Layout</a></li>
-                </ul>
-            </nav>
-        </div>
-            <a className="navbar-item" href="/">
-            <span className="icon">
-                  <FontAwesomeIcon icon={faHome}/>
-                </span>
-                <div>Home</div>
-            </a>
-            <a className="navbar-item" href="/Profil">
-            <span className="icon">
-                  <FontAwesomeIcon icon={faUser}/>
-                </span>
-                <div>Profil</div>
-            </a>
-            <div className="navbar-item">
-              <Modal> </Modal>
-            </div>
-            <span className="navbar-item">
-              <a className="button is-primary is-inverted" href='/Login'>
-                <span className="icon">
-                  <FontAwesomeIcon icon={faSignInAlt}/>
-                </span>
-                <span>Login</span>
-              </a>
-            </span>
-          </div>
-    </nav>
-  </div>
-  <div className="hero-body">
-    <div className="container has-text-centered">
-      <p className="title">
-        Workout-Tracker
-      </p>
-      <p className="subtitle">
-        Time for Sport
-      </p>
-    </div>
-  </div>
 </section>
 </div>
 export default Head;
-
