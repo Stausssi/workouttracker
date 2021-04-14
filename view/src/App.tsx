ab6eaa2976af371c81cc62998d3747f9be9418e1
Simon <stauss.simon.01@gmail.com>
2021-04-14 22:42:36 +0200

    Added backend url constant

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -1,4 +1,4 @@
-import {Route,BrowserRouter} from 'react-router-dom'
+import {Route, BrowserRouter} from 'react-router-dom'
 import Homepage from './Homepage';
 import Second from './Second';
 import Login from './Login';
@@ -8,16 +8,17 @@ import "bulma"
 import 'bulma-extensions/dist/css/bulma-extensions.min.css'
 
 export const App = () =>
-<BrowserRouter>
-<div className="App"> 
-    <div className="children">
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/Second" component={Second} />
-        <Route exact path="/Login" component={Login} />
-        <Route exact path="/Profil" component={Profil} />
-    </div>
-</div>
-</BrowserRouter>;
+    <BrowserRouter>
+        <div className="App">
+            <div className="children">
+                <Route exact path="/" component={Homepage}/>
+                <Route exact path="/Second" component={Second}/>
+                <Route exact path="/Login" component={Login}/>
+                <Route exact path="/Profil" component={Profil}/>
+            </div>
+        </div>
+    </BrowserRouter>;
 
 export default App;
 
+export const BACKEND_URL = "http://localhost:9000/backend/";
--- view/src/Body.tsx
+++ view/src/Body.tsx
@@ -1,49 +1,52 @@
 import React from 'react';
 import './css/App.css';
-import ToggleButton from'./ToggleButton'
+import ToggleButton from './ToggleButton'
 import 'bulma-extensions/bulma-switch/dist/css/bulma-switch.min.css'
+import {BACKEND_URL} from "./App";
 
 interface Props {
 }
+
 interface State {
-  showPopup:boolean
-  active:boolean
-  apiResponse:any
+    showPopup: boolean
+    active: boolean
+    apiResponse: any
 }
 
-export default class Body extends React.Component<Props,State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      showPopup: false,
-      active: false,
-      apiResponse: ""
-    };
-  }
-  callAPI() {
-		fetch("http://157.90.160.201:9000/backend/testConnection")
-			.then(res => res.text())
-			.then(res => this.setState({ apiResponse: res }));
-	}
+export default class Body extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            showPopup: false,
+            active: false,
+            apiResponse: ""
+        };
+    }
 
-	componentWillMount() {
-		this.callAPI();
-	}
+    callAPI() {
+        fetch(BACKEND_URL + "testConnection")
+            .then(res => res.text())
+            .then(res => this.setState({apiResponse: res}));
+    }
 
-  render () {  
-    return(
-    <section id="body-content">
-    <div id="col-1">
-    <h1>This is half of a page</h1>
-    <p>{this.state.apiResponse}</p>
-    <div>
-    <ToggleButton />
-    </div>
-    </div>
-    <div id="col-2">
-    <h1>Hier kommen Statistiken, Analytik, Charts und Kalender</h1>
-    </div>
-  </section>
-    )
-  }
+    componentWillMount() {
+        this.callAPI();
+    }
+
+    render() {
+        return (
+            <section id="body-content">
+                <div id="col-1">
+                    <h1>This is half of a page</h1>
+                    <p>{this.state.apiResponse}</p>
+                    <div>
+                        <ToggleButton/>
+                    </div>
+                </div>
+                <div id="col-2">
+                    <h1>Hier kommen Statistiken, Analytik, Charts und Kalender</h1>
+                </div>
+            </section>
+        )
+    }
 }
