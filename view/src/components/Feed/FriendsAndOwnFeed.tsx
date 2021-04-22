623914fb8d39bad6865fce963c03f258e691c930
n1klasD <67094918+n1klasD@users.noreply.github.com>
2021-04-22 16:07:24 +0200

    frontend for feed display

--- view/src/components/Feed/FriendsAndOwnFeed.tsx
+++ view/src/components/Feed/FriendsAndOwnFeed.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import {BACKEND_URL} from "../../App";
+import SessionHandler from "../../utilities/SessionHandler";
 
 type postData = {
     likes: number
@@ -9,9 +11,10 @@ type postData = {
 }
 
 interface FeedState {
-    //postData: postData,
+    postData: postData[] | [],
     //comments: string,
     loaded: boolean
+    offset_page: number
 }
 
 interface EmptyProps {
@@ -22,20 +25,37 @@ export class OwnFeed extends React.Component<EmptyProps, FeedState> {
     constructor(props: EmptyProps) {
         super(props);
         this.state = {
-            loaded: false
+            loaded: false,
+            postData: [],
+            offset_page: 0
         }
     }
 
     componentDidMount() {
         // fetch X activities from Backend
-        // render x ActivityBoxes based on them --> insert information
-
+        this.getFeed()
         //set loaded to true
         this.setState({loaded: true});
     }
 
-    renderFeed (){
-        return <ActivityBox />;
+    getFeed() {
+        fetch(BACKEND_URL + "feed/own?offset=" + this.state.offset_page, {
+            method: "GET",
+            headers: {
+                Accepts: "application/json",
+                Authorization: SessionHandler.getAuthToken()
+            }
+        }).then((response) => {
+            if (response.ok) {
+                return response.json().then(response => {
+                    this.setState({postData: response["activities"]});
+                });
+            }
+        });
+    }
+
+    renderFeed() {
+        return this.state.postData.map((activity: postData) => <div><ActivityBox postData={activity}/><br/></div>);
     }
 
     render() {
@@ -45,14 +65,14 @@ export class OwnFeed extends React.Component<EmptyProps, FeedState> {
                     <div>Hier kommt die eigene Aktivität hin</div>
                     <div>Weitere eigene Komponenten</div>
 
-                    {this.state.loaded ? this.renderFeed(): <p>not Working</p>}
+                    {this.state.loaded ? this.renderFeed() : <p>not Working</p>}
                 </div>
             </>
         );
     }
 }
 
-export class FriendsFeed extends React.Component<{ }, { }> {
+export class FriendsFeed extends React.Component<{}, {}> {
     render() {
         return (
             <>
@@ -64,9 +84,10 @@ export class FriendsFeed extends React.Component<{ }, { }> {
 }
 
 //Component for an activity Box, to contain a like button, comment section and an activity table
-class ActivityBox extends React.Component<any, any>{
+class ActivityBox extends React.Component<{ postData: postData }, any> {
     render() {
-        return(
+        const props = this.props.postData;
+        return (
             <>
                 <div className="card">
                     <div className="card-content">
@@ -75,33 +96,34 @@ class ActivityBox extends React.Component<any, any>{
                                 <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image"/>
                             </figure>
                             <div className="media-content has-text-left	pl-2">
-                                <p className="title is-4">Sportart</p>
+                                <p className="title is-4">{props.sport}</p>
                                 <p className="subtitle is-6">
-                                    <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016 </time>
-                                    - Niklas Drössler
+                                    <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                                    - {props.username}
                                 </p>
                             </div>
                             <div className="">
-                                <button className="button is-success is-rounded">Like</button>
+                                <button className="button is-success is-rounded">Like {props.likes}</button>
                             </div>
                         </div>
 
                         <div className="content ">
-                            <ActivityTable activityData={{ duration: 200, pace: 20, averageHeartRate: 50, altitudeDifference: 10}}/>
+                            <ActivityTable
+                                activityData={props.activityData}/>
                         </div>
                     </div>
-                        <div className="card">
-                            <header className="card-header">
-                                <p className="card-header-title">
-                                    Comments
-                                </p>
-                                <button className="card-header-icon" aria-label="more options">
+                    <div className="card">
+                        <header className="card-header">
+                            <p className="card-header-title">
+                                Comments
+                            </p>
+                            <button className="card-header-icon" aria-label="more options">
                                 <span className="icon">
                                     <i className="fas fa-angle-down" aria-hidden="true"/>
                                 </span>
-                                </button>
-                            </header>
-                        </div>
+                            </button>
+                        </header>
+                    </div>
                 </div>
             </>
         );
@@ -121,27 +143,27 @@ interface props {
 }
 
 //displays an activity table
-class ActivityTable extends React.Component<props, { }>{
+class ActivityTable extends React.Component<props, {}> {
     constructor(props: props) {
         super(props);
     }
 
-    renderTableHeaders(activityData: activityData){
+    renderTableHeaders(activityData: activityData) {
         let keys = Object.keys(activityData) //get property keys
         return keys.map((key) => <th>{key}</th>)
     }
 
-    renderTableContents(activityData: activityData){
+    renderTableContents(activityData: activityData) {
         let values = Object.values(activityData) //get property values
         return values.map((value) => <th>{value}</th>)
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <>
                 <table className="table is-narrow">
                     <thead>
-                        {this.renderTableHeaders(this.props.activityData)}
+                    {this.renderTableHeaders(this.props.activityData)}
                     </thead>
                     <tbody>
                     <tr>
