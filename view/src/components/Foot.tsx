f5e3d56ef4c634c16462703606061c0f6aed7f2c
Simon <stauss.simon.01@gmail.com>
2021-05-02 15:38:01 +0200

    added icon copyright

--- view/src/components/Foot.tsx
+++ view/src/components/Foot.tsx
@@ -7,6 +7,7 @@ export const Footer = () => (
                 Informationen für <a href="/#">Homepage</a>. The source code is licensed
                 <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>. Für
                 weitere Informationen, bitte kontaktieren Sie den Admin.
+                Use <a href="https://icons8.com/"> Icons8</a> Icons.
             </p>
         </div>
     </footer>
