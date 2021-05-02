02f4b9c3006e2a1e4f5468a78f68f74712a510ea
n1klasD <67094918+n1klasD@users.noreply.github.com>
2021-05-02 15:34:28 +0200

    changed navbar

--- view/src/components/Head.tsx
+++ view/src/components/Head.tsx
@@ -10,50 +10,41 @@ import NewLogo from "../img/TransarentLogo.png";
 
 export const Head = () => (
     <div id="head">
-        <section className="hero is-dark is-small">
-            <div className="hero-head">
-                <nav className="navbar">
-                    <img src={NewLogo} alt="Logo" width={250}/>
-                    <div
-                        className="container has-text-centered is-justify-content-center is-align-items-center is-flex-grow-1">
-                        <p className="title">Workout-Tracker</p>
-                    </div>
-                    <div className="navbar-end">
-                        <div className="navbar-item">
-                            <SearchBar/>
-                        </div>
-                        <Link className="navbar-item" to="/">
-                            <span className="icon">
-                              <FontAwesomeIcon icon={faHome}/>
-                            </span>
-                            <div>Home</div>
-                        </Link>
-                        <Link className="navbar-item" to="/profile">
-                            <span className="icon">
-                                <FontAwesomeIcon icon={faUser}/>
-                            </span>
-                            <div>Profile</div>
-                        </Link>
-                        <div className="navbar-item">
-                            <ActivityModal/>
-                        </div>
-                        <span className="navbar-item">
-                          <Link
-                              className="button is-primary is-inverted"
-                              to={"/login"}
-                              onClick={() => SessionHandler.logOut()}
-                          >
-                              <span className="icon">
-                                  <FontAwesomeIcon icon={faSignInAlt}/>
-                              </span>
-                              <span>Logout</span>
-                          </Link>
+        <nav className="navbar p-2 is-dark">
+            <img src={NewLogo} alt="Logo" width={250}/>
+            <div className="navbar-end">
+                <div className="navbar-item">
+                    <SearchBar/>
+                </div>
+                <Link className="navbar-item" to="/">
+                    <span className="icon">
+                        <FontAwesomeIcon icon={faHome}/>
+                    </span>
+                    <div>Home</div>
+                </Link>
+                <Link className="navbar-item" to="/profile">
+                    <span className="icon">
+                        <FontAwesomeIcon icon={faUser}/>
+                    </span>
+                    <div>Profile</div>
+                </Link>
+                <div className="navbar-item">
+                    <ActivityModal/>
+                </div>
+                <span className="navbar-item">
+                    <Link
+                        className="button is-primary is-inverted"
+                        to={"/login"}
+                        onClick={() => SessionHandler.logOut()}>
+
+                        <span className="icon">
+                            <FontAwesomeIcon icon={faSignInAlt}/>
                         </span>
-                    </div>
-                </nav>
+                        <span>Logout</span>
+                    </Link>
+                </span>
             </div>
-            <div className="hero-body"/>
-        </section>
+        </nav>
     </div>
 );
 export default Head;
