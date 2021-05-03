9e132af897805728a876e106b9b9d5e46180e1e9
Simon <stauss.simon.01@gmail.com>
2021-05-03 16:55:26 +0200

    changed footer content

--- view/src/components/Foot.tsx
+++ view/src/components/Foot.tsx
@@ -4,10 +4,9 @@ export const Footer = () => (
     <footer id="footer" className="p-5">
         <div className="content has-text-centered">
             <p>
-                Informationen für <a href="/#">Homepage</a>. The source code is licensed
-                <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>. Für
-                weitere Informationen, bitte kontaktieren Sie den Admin.
-                Use <a href="https://icons8.com/"> Icons8</a> Icons.
+                The source code is licensed <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>.
+                Please contact the admin to receive further information.
+                Uses <a href="https://icons8.com/"> Icons8</a> Icons.
             </p>
         </div>
     </footer>
